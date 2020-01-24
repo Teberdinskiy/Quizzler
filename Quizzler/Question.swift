e28a45a5e6905bb9aba22a98ae01056f0972afa6
Alikhan Batchaev <loko-han@mail.ru>
2020-01-24 22:01:48 +0300

    создал новый файл новая структура Question

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by 🧔🏻Alikhan Batchaev on 24.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let answer: String
+    let text: String
+}
